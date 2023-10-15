9830c9f9a62554f936543137caeb052b8a048a37
0LaMica <mica.tui@proton.me>
2023-10-15 22:17:51 +0200

    Fixing gtk config again

--- Home/Desktop/Hyprland/Themes/gtk/gtk.nix
+++ Home/Desktop/Hyprland/Themes/gtk/gtk.nix
@@ -1,18 +1,19 @@
 {config, pkgs, ...}:{
-    gtk = {
-      enable = true;
-      theme = {
-          name = "Fluent-Dark";
-          package = pkgs.fluent-gtk-theme;
-        };
-      iconTheme = {
+  gtk = {
+     enable = true;
+     theme = {
+        name = "Fluent-Dark";
+        package = pkgs.fluent-gtk-theme;
+    };
+    iconTheme = {
           name = "ePapirus-Dark";
           package = pkgs.papirus-icon-theme;
-        };
     };
+   };
     dconf.settings = {
-    "org/gnome/desktop/interface" = {
-       color-scheme = "prefer-dark";
-  };
+      "org/gnome/desktop/interface" = {
+         color-scheme = "prefer-dark";
+      };
+    };
     home.file.".icons/default".source = "${pkgs.bibata-cursors}/share/icons/Bibata-Modern-Classic";
 }
