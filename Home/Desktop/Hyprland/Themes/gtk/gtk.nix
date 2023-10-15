e259f40aa0b945c04632c29905b2c16ace46e328
0LaMica <mica.tui@proton.me>
2023-10-15 22:08:43 +0200

    Fixing gtk config

--- Home/Desktop/Hyprland/Themes/gtk/gtk.nix
+++ Home/Desktop/Hyprland/Themes/gtk/gtk.nix
@@ -2,7 +2,7 @@
     gtk = {
       enable = true;
       theme = {
-          name = "Fluent-dark-float-blur";
+          name = "Fluent-dark";
           package = pkgs.fluent-gtk-theme;
         };
       iconTheme = {
