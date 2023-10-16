3280ed6f033fed48cf92beeb8f6a153f55a3703a
0LaMica <mica.tui@proton.me>
2023-10-16 05:32:29 +0200

    Changed gtk theme

--- Home/Desktop/Hyprland/Themes/gtk/gtk.nix
+++ Home/Desktop/Hyprland/Themes/gtk/gtk.nix
@@ -2,18 +2,34 @@
   gtk = {
      enable = true;
      theme = {
-        name = "Fluent-Dark";
-        package = pkgs.fluent-gtk-theme;
+        name = "Orchis-Dark";
+        package = pkgs.orchis-theme;
     };
     iconTheme = {
           name = "ePapirus-Dark";
           package = pkgs.papirus-icon-theme;
     };
+    cursorTheme = {
+      name = "Bibata-Modern-Classic";
+      package = pkgs.bibata-cursors;
+    };
+    gtk3.extraConfig = {
+      settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+    gtk4.extraConfig = {
+      settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
    };
     dconf.settings = {
       "org/gnome/desktop/interface" = {
          color-scheme = "prefer-dark";
       };
     };
+
     home.file.".icons/default".source = "${pkgs.bibata-cursors}/share/icons/Bibata-Modern-Classic";
 }
