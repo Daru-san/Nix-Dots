b47fd42517750c1ecc20477d7f277da7fe11ac17
0LaMica <mica.tui@proton.me>
2023-10-28 14:14:38 +0200

    Experiment with python packages

--- Home/Programs/coding/Python/python.nix
+++ Home/Programs/coding/Python/python.nix
@@ -1,5 +1,13 @@
 {config,pkgs,...}:{
   home.packages = with pkgs; [
-       (python311.withPackages(ps: with ps; [ pygments pathlib configargparse setuptools pygit2 sh aioshutil pip]))
+       (python311.withPackages(ps: with ps; [ 
+         pygments
+    #     pathlib 
+    #     configargparse 
+    #     setuptools 
+    #     pygit2 
+    #     sh 
+    #     aioshutil 
+        pip]))
   ];
-  }
+}
