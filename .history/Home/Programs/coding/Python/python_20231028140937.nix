2a40bbcd84792b342756afce8617fcdbd0afa780
0LaMica <mica.tui@proton.me>
2023-11-03 10:39:30 +0200

    Cleaned up redundant files

--- .history/Home/Programs/coding/Python/python_20231028140937.nix
+++
@@ -1,5 +0,0 @@
-{config,pkgs,...}:{
-  home.packages = with pkgs; [
-       (python311.withPackages(ps: with ps; [ pygments setuptools pygit2 sh aioshutil pip]))
-  ];
-  }
--- .history/Home/Programs/coding/Python/python_20231028141047.nix
+++
@@ -1,5 +0,0 @@
-{config,pkgs,...}:{
-  home.packages = with pkgs; [
-       (python311.withPackages(ps: with ps; [ pygments pathlib configargparse setuptools pygit2 sh aioshutil pip]))
-  ];
-  }
