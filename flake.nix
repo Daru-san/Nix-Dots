2493a18023eb5030ea270653c0d567f53e0bf093
0LaMica <mica.tui@proton.me>
2023-11-05 17:56:39 +0200

    Testing

--- flake.nix
+++ flake.nix
@@ -2,14 +2,17 @@
   description = "My NixOS home-manager configuration ft. Hyprland";
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    nur.url = "github:nix-community/NUR";
+    nur.url = github:nix-community/NUR;
     spicetify-nix.url = github:the-argus/spicetify-nix;
     home-manager = {
       url = github:nix-community/home-manager;
       # home manager use our nixpkgs and not its own
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    hyprland.url = "github:hyprwm/Hyprland/v0.31.0";
+    hyprland = {
+      url = github:hyprwm/Hyprland;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
