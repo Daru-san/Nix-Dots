e263529aa56b0a7892efcbdd6b3cc37b27609faa
0LaMica <mica.tui@proton.me>
2023-11-08 16:46:46 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,21 +1,22 @@
 {
-  description = "My NixOS home-manager configuration ft. Hyprland";
+  description = "My NixOS home-manager flake, ft. Hyprland, spicetify and others ";
+  #Here's this little comment I'm gonna add here just because I can
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    nur.url = github:nix-community/NUR;
-    spicetify-nix.url = github:the-argus/spicetify-nix;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable; #Pin to nixpkgs unstable
+    nur.url = github:nix-community/NUR; #Add nur repo for firefox plugins
+    spicetify-nix.url = github:the-argus/spicetify-nix; #Add spicetify to flake
     home-manager = {
       url = github:nix-community/home-manager/master;
       # home manager use our nixpkgs and not its own
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland = {
+      #Add Hyprland to home config
       url = github:hyprwm/Hyprland;
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
+    nix-doom-emacs.url = "github:nix-community/nix-doom-emacs"; #Add doom-emacs to config
   };
-
   outputs = {
     self,
     nixpkgs,
@@ -33,27 +34,26 @@
     genSystems = nixpkgs.lib.genAttrs supportedSystems;
     pkgs = genSystems (system: import nixpkgs {inherit system;});
     modules = [
-        hyprland.homeManagerModules.default
+        hyprland.homeManagerModules.default #Enable Hyprland
         {wayland.windowManager.hyprland.enable = true;}
         {nixpkgs.overlays = [ nur.overlay ];}
     ];
     config = {
       allowUnfree = true;
     };
-
     stateVersion = "unstable";
   in {
     packages = genSystems (system: {
       homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
         pkgs = pkgs.${system};
         modules = [
-          ./Home/default.nix
+          ./Home/default.nix #Add home config
           {nixpkgs.overlays = [ nur.overlay ];}
           {imports = [
             nix-doom-emacs.hmModule
           ];
            programs.doom-emacs = {
-            enable = true;
+            enable = false; #Disable for now to be re-enabled later on
             doomPrivateDir = ./Home/Programs/cli/emacs/doom.d;
             };
           }
