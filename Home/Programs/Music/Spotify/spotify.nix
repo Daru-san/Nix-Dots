7e0ffaef55903bb42541085c7703633ff33a5f27
0LaMica <mica.tui@proton.me>
2023-11-09 14:38:19 +0200

    Update spicetify theme

--- Home/Programs/Music/Spotify/spotify.nix
+++ Home/Programs/Music/Spotify/spotify.nix
@@ -12,13 +12,33 @@ in
   imports = [ spicetify-nix.homeManagerModule ];
 
   # configure spicetify :)
-  programs.spicetify =
+  programs.spicetify = let
+    officialThemes = pkgs.fetchgit {
+      url = "https://github.com/spicetify/spicetify-themes";
+      sha256 = "0sab8jh4867fh66zdz7msg1bwwqpm0zc6y20hw7328p7agcr7xlk";
+      rev = "2e124a90561649a8aa410e89c6893e5b5d4816b9";
+    };
+  in 
     {
 #Enable spicetify
       enable = true;
 #Spicetify theme
-      theme = spicePkgs.themes.Dribbblish;
-      colorScheme = "lunar";
+      theme = {
+        name = "text";
+        src = officialThemes;
+        appendName = true;
+        injectCss = true;
+        replaceColors = true;
+        overwriteAssets = true;
+        sidebarConfig = true;
+        patches = {
+          "xpui.js_find_0880" = "COLLAPSED\?64:32";
+          "xpui.js_repl_0880" = "COLLAPSED?32:32";
+          "xpui.js_find_8008" = ",(\w+=)56,";
+          "xpui.js_repl_8008" = ",$1}32,";
+        };
+      };
+      colorScheme = "Kanagawa";
 
       enabledExtensions = with spicePkgs.extensions; [
 #Extensions for spicetify
@@ -36,7 +56,6 @@ in
         savePlaylists
         adblock
         shuffle # shuffle+ (special characters are sanitized out of ext names)
-        hidePodcasts
         lastfm
         playlistIcons
         goToSong
