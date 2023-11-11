8d2983be61436fcf7b933d61330a3bc672ebd1b3
0LaMica <mica.tui@proton.me>
2023-11-11 19:22:01 +0200

    Update firefox nix config

--- Home/Programs/Browser/Firefox/default.nix
+++ Home/Programs/Browser/Firefox/default.nix
@@ -176,8 +176,8 @@
           #Firefox-one configs
           "firefoxone.rhythm_sound_tab" = true;
           "firefoxone.selected_tab_gradient_border" = true;
-          "firefoxone.style_without_leftsidebar" = true;
-          "firefoxone.tree_tabs_style" = true;
+          "firefoxone.style_without_leftsidebar" = false;
+          "firefoxone.tree_tabs_style" = false;
           "firefoxone.without-default-colors" = true;
           "firefoxone.main-image" = false;
         };
