79e6155579f6e5cf7b7387aa2da1f8f459857caf
0LaMica <mica.tui@proton.me>
2023-11-12 08:18:57 +0200

    Update shell aliases

--- Home/Settings/Shell/default.nix
+++ Home/Settings/Shell/default.nix
@@ -7,16 +7,25 @@
   ];
 
   #Global shell aliases
-  home.shellAliases = {
-    g = "git";
+  home.shellAliases = let
+    git = "${config.programs.git.package}/bin/git";
+    neofetch = "${pkgs.neofetch}/bin/neofetch";
+    tty-clock = "${pkgs.tty-clock}/bin/tty-clock";
+    kitty = "${config.programs.kitty.package}/bin/kitty";
+    ranger = "${pkgs.ranger}/bin/ranger";
+    ani-cli = "${pkgs.ani-cli}/bin/ani-cli";
+    w3m = "${pkgs.w3m}/bin/w3m";
+    neovim = "${config.programs.neovim.package}/bin/nvim";
+  in {
+    g = "${git}";
     ll = "ls -l";
     ex = "exit";
     cat = "ccat";    
     logout = "loginctl terminate-user $USER";
-    nofetch = "neofetch --config none"; 
+    nofetch = "${neofetch} --config none"; 
     O_O = "echo Hi!";
-    clock = "tty-clock -b -s -c -B -r -n -S";
-    nv = "nvim";
+    clock = "${tty-clock} -b -s -c -B -r -n -S";
+    nv = "${neovim}";
     rsync-copy = "rsync -avzhe --progress";
     rsync-move = "rsync --remove-source-files -zvh --progress";
     nix-rebuild = "sudo nixos-rebuild switch";
@@ -24,9 +33,9 @@
     nix-editHW = "sudoedit /etc/nixos/hardware-configuration.nix";
     rs = "reset";
     cl = "clear";
-    anifetch = "neofetch --config ~/.config/neofetch/anifetch.conf";
+    anifetch = "${neofetch} --config ~/.config/neofetch/anifetch.conf";
     img = "kitten icat --hold";
-    r = "ranger";
+    r = "${ranger}";
     home-flake = "home-manager switch --flake ~/Nix-Dots --impure";
     nix-upgrade = "sudo nixos-rebuild switch --upgrade-all";
     nix-upgrade-full = "sudo nixos-rebuild switch --upgrade-all && home-flake";
@@ -34,10 +43,11 @@
     cdownl = "cd ~/Downloads/";
     cdocs = "cd ~/Documents/";
     cwalls = "cd ~/Wallpapers/";
-    ani = "ani-cli --rofi --vlc";
-    anidu = "ani-cli  --rofi --vlc --dub";
-    anid = "ani-cli -d";
-    anidud = "ani-cli --dub -d";
+    ani = "${ani-cli} --rofi --vlc";
+    anidu = "${ani-cli}  --rofi --vlc --dub";
+    anid = "${ani-cli} -d";
+    anidud = "${ani-cli} --dub -d";
     rebootf = "sudo systemctl reboot -f";
+    nix-manual = "${w3m}/bin/w3m file:///nix/store/cvnacwjawy7fi41q5ffklvxcjnhnib3k-nixos-manual-html/share/doc/nixos/index.html";
   };
 }
