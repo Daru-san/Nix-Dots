0986e3ed9f38ab6629016787e3069eb13d2f9855
0LaMica <mica.tui@proton.me>
2023-11-12 09:26:00 +0200

    Add module for cava

--- Home/Programs/Music/default.nix
+++ Home/Programs/Music/default.nix
@@ -3,10 +3,6 @@
   imports = [
     ./Spotify/spotify.nix
     ./cmus/cmus.nix
-  ];
-  home.packages = with pkgs; [
-#Packages for music
-    cava
-    ytui-music    
+    ./cava/default.nix
   ];
 }
