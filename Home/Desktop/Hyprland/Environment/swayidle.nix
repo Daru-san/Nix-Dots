e102685f7d50abbf2f58a1437bcff3c40481f721
0LaMica <mica.tui@proton.me>
2023-11-13 22:58:51 +0200

    Update swayidle

--- Home/Desktop/Hyprland/Environment/swayidle.nix
+++ Home/Desktop/Hyprland/Environment/swayidle.nix
@@ -3,11 +3,11 @@
     services.swayidle = {
     enable = true;
     events = [
-      { event = "before-sleep"; command = "${pkgs.swaylock}/bin/swaylock -fF"; }
+      { event = "before-sleep"; command = "${config.programs.swaylock.package}/bin/swaylock -fF"; }
       { event = "lock"; command = "lock"; }
     ];
     timeouts = [
-      { timeout = 400; command = "${pkgs.swaylock}/bin/swaylock -fF";}
+      { timeout = 400; command = "${config.programs.swaylock.package}/bin/swaylock -fF";}
       { timeout = 1200; command = "systemctl suspend"; }
     ];
     systemdTarget = "xdg-desktop-portal-hyprland.service";
