3e8827c4fba8dc6aed4f72882ae815970e931f9c
0LaMica <mica.tui@proton.me>
2023-11-14 00:06:15 +0200

    Update hyprland config

--- Home/Desktop/Hyprland/Environment/default.nix
+++ Home/Desktop/Hyprland/Environment/default.nix
@@ -12,12 +12,13 @@
     ./swayidle.nix
   ];
   home.packages = with pkgs; [
+    wl-clipboard
     swaynotificationcenter
-    hyprpicker #I need to get this to work with extensions
-    cool-retro-term #Nice terminal
+    hyprpicker #I need to get this to work with notifications
+    wtype 
   ];
   home.sessionVariables = {
+    #Make kitty the default terminal
     TERMINAL = "${config.programs.kitty.package}/bin/kitty";
-#Make kitty default terminal
   };
 }
